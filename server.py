e4bb4ca281edada259bd1e9007ebc9ffa5bbbe13
urikxx <zxasv4402@gmail.com>
2024-11-11 21:53:52 +0300

    add CORS policy

--- server.py
+++ server.py
@@ -3,12 +3,15 @@ from io import BytesIO
 from PIL import Image
 from flask import Flask, request, jsonify, send_file, Response
 from flask_api import status
+from flask_cors import CORS, cross_origin
 
 from Embedding import Embedding
 from domain import Status
 from storage import HDF5ImageStorage
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB
 
 
@@ -21,12 +24,12 @@ def _send_image(image: Image):
     # Отправляем изображение как файл для скачивания
     return send_file(img_io, mimetype='image/png', as_attachment=True, download_name=f'{image.uuid}.png')
 
-
 def create_app():
     storage = HDF5ImageStorage()
     embedding = Embedding(storage)
 
     @app.route('/images/upload', methods=['POST'])
+    @cross_origin()
     def upload_image():
         if 'file' not in request.files:
             return jsonify({'error': 'No file uploaded'}), status.HTTP_400_BAD_REQUEST
@@ -43,6 +46,7 @@ def create_app():
         return Response(status=status.HTTP_200_OK)
 
     @app.route('/images/<string:image_id>/download', methods=['GET'])
+    @cross_origin()
     def get_image(image_id):
         try:
             return _send_image(embedding.get_processed_image(image_id))
@@ -52,6 +56,7 @@ def create_app():
             return jsonify({'error': str(e)}), status.HTTP_500_INTERNAL_SERVER_ERROR
 
     @app.route('/images/<string:image_id>/count', methods=['POST'])
+    @cross_origin()
     def start_counting(image_id: str):
         try:
             embedding.handle_algorithm(image_id)
@@ -62,6 +67,7 @@ def create_app():
             return jsonify({'error': str(e)}), status.HTTP_500_INTERNAL_SERVER_ERROR
 
     @app.route('/counts/<string:job_id>', methods=['GET'])
+    @cross_origin()
     def get_job_status(job_id):
         try:
             job = embedding.get_job_by_uuid(job_id)
@@ -71,6 +77,7 @@ def create_app():
             return jsonify({'error': 'Job not found'}), status.HTTP_404_NOT_FOUND
 
     @app.route('/counts', methods=['GET'])
+    @cross_origin()
     def get_job_list():
         try:
             return jsonify([job.to_dict() for job in embedding.get_jobs()])
@@ -78,6 +85,7 @@ def create_app():
             return jsonify({'error': str(e)}), status.HTTP_500_INTERNAL_SERVER_ERROR
 
     @app.route('/counts/<string:job_id>/image/download', methods=['GET'])
+    @cross_origin()
     def get_segmented_image(job_id):
         try:
             job = embedding.get_job_by_uuid(job_id)
@@ -90,6 +98,7 @@ def create_app():
             return jsonify({'error': str(e)}), status.HTTP_500_INTERNAL_SERVER_ERROR
 
     @app.route('/images', methods=['GET'])
+    @cross_origin()
     def get_image_list():
         # page = request.args.get('page', 1, type=int)
         # per_page = request.args.get('per_page', 10, type=int)
@@ -101,6 +110,7 @@ def create_app():
         return images
 
     @app.route('/algorithms', methods=['GET'])
+    @cross_origin()
     def get_algorithm_list():
         return jsonify("Canny")
 
